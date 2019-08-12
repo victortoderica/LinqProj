2395cf0633ee89fab84983044b9e41ca3c77e0d9
VICTOR <victor.toderica@gmail.com>
2019-08-12 22:52:40 +0300

    1

--- LinqTests/LinqTests/Program.cs
+++ LinqTests/LinqTests/Program.cs
@@ -13,4 +13,21 @@ namespace LinqTests
             Console.WriteLine("Just testing");
         }
     }
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined { get; set; }
+    }
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Headquarters { get; set; }
+        public int Year { get; set; }
+    }
 }
